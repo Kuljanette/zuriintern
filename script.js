258e30802ffea9160be5dd2dec9663cd3eb89bf2
Janet Kulaba <139434715+Kuljanette@users.noreply.github.com>
2023-09-11 10:45:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function updateDateTime(){
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDate = new Date();
+
+
+
+    const currentDay = daysOfWeek[currentDate.getUTCDay()];
+
+    const currentUTC = currentDate.getTime();
+
+
+
+    document.getElementById("currentDay").textContent = currentDay;
+
+
+    document.getElementById("currentUTC").textContent = currentUTC;
+
+
+}
+
+updateDateTime();
+setInterval(updateDateTime, 1000);
+    
